2d2aabac79d3baa83a332ec5514d1acaa3ed0794
Tina Wulff <tina@tinawulff.dk>
2025-06-13 10:14:14 +0200

    footer more

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,6 +3,21 @@
 export default function footer() {
 
     return (
+        <>
+        <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About Us</li>
+        </ul>
+
+        <ul>
+            <li>Returns & Refunds</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+            <li>Terms & Conditions</li>
+        </ul>
+
         
+        </>
     )
 }
