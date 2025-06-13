2546e8f822c597eb97cb41fcaea1bee08748d1cb
Tina Wulff <tina@tinawulff.dk>
2025-06-13 14:09:06 +0200

    footer styling desktop done

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,48 +12,48 @@ import { FaCcMastercard } from "react-icons/fa";
 export default function Footer() {
 
     return (
-        <footer className='bg-black text-white grid grid-cols-3 gap-y-8'>
+        <footer className='bg-black text-white grid grid-cols-3 gap-y-8 px-4'>
 
-            <div className='footer__nav col-span-3 grid grid-cols-2'>
+            <div className='footer__nav col-span-3 grid grid-cols-2 mt-10 px-10'>
                 <ul>
-                    <li><NavLink to='/'>Home</NavLink></li>
-                    <li> Shop</li>
-                    <li><NavLink to='/about'>About Us</NavLink></li>
+                    <li className='mb-2'><NavLink to='/'>Home</NavLink></li>
+                    <li className='mb-2'> Shop</li>
+                    <li className='mb-2'><NavLink to='/about'>About Us</NavLink></li>
                 </ul>
 
                 <ul>
-                    <li><NavLink to='/faq'> Returns & Refunds</NavLink></li>
-                    <li><NavLink to='/faq'>Delivery</NavLink></li>
-                    <li><NavLink to='/faq'>Privacy Policy</NavLink></li>
-                    <li><NavLink to='/faq'>Terms & Conditions</NavLink></li>
+                    <li className='mb-2'><NavLink to='/faq'> Returns & Refunds</NavLink></li>
+                    <li className='mb-2'><NavLink to='/faq'>Delivery</NavLink></li>
+                    <li className='mb-2'><NavLink to='/faq'>Privacy Policy</NavLink></li>
+                    <li className='mb-2'><NavLink to='/faq'>Terms & Conditions</NavLink></li>
                 </ul>
 
 
                 <div className='col-3'>
-                    <h2>Contact</h2>
-                    <p>2 Joppa Rd, Edinburgh, EH15 2EU</p>
-                    <p><FaPhone /> 0131 556 7901</p>
+                    <h2 className='mb-2  text-xl'>Contact</h2>
+                    <p className='leading-10'>2 Joppa Rd, Edinburgh, EH15 2EU</p>
+                    <p className='flex gap-4 mb-4'><FaPhone /> 0131 556 7901</p>
 
-                    <p>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</p>
-                    <p><FaPhone /> 01324 629 011</p>
+                    <p className='leading-10'>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</p>
+                    <p className='flex gap-4'><FaPhone /> 01324 629 011</p>
 
-                    <ul className='flex gap-2'>
-                        <li className=''><FaFacebookSquare /></li>
-                        <li><FaTwitterSquare /></li>
-                        <li><FaSquareInstagram /></li>
-                        <li><IoLogoYoutube /></li>
+                    <ul className='flex gap-2 mb-4 mt-4'>
+                        <li className='text-2xl'><FaFacebookSquare /></li>
+                        <li className='text-2xl'><FaTwitterSquare /></li>
+                        <li className='text-2xl'><FaSquareInstagram /></li>
+                        <li className='text-2xl'><IoLogoYoutube /></li>
                     </ul>
                 </div>
             </div>
 
-            <div className='row-2 col-span-3 border-t'>
-                <ul className='flex gap-2'>
+            <div className='row-2 col-span-3 border-t px-10 mb-4'>
+                <ul className='flex gap-2 mt-2 mb-2'>
                     <li className='text-4xl'><FaCcStripe /></li>
                     <li className='text-4xl'><FaCcVisa /></li>
                     <li className='text-4xl'><FaCcMastercard /></li>
                 </ul>
-                <p>HiFi Horizon (Edinburgh) Ltd is registered in Scotland. No: SC049298. Registered office: 2 Joppa Rd, Edinburgh EH15 2EU</p>
-                <p>Designed by WU07 </p>
+                <small className='col-span-2'>HiFi Horizon (Edinburgh) Ltd is registered in Scotland. No: SC049298. Registered office: 2 Joppa Rd, Edinburgh EH15 2EU</small>
+                <small className='col-3'>Designed by WU07</small>
             </div>
         </footer>
     )
