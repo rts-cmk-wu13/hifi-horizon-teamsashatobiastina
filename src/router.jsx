150da220761f8bf02e09c604885e92a48822338c
Tobias <tobimation23@gmail.com>
2025-06-13 08:49:39 +0200

    added a router (though it probably needs some tinkering and work)

---
+++ src/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import Layout from "./Layout";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+       
+       
+       
+
+        ]
+    }
+
+]);
+export default router;
