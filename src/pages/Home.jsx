f8b002fc317e91cddc944881f8df44e7c5463998
Tobias <tobimation23@gmail.com>
2025-06-16 12:27:33 +0200

    home commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
+import Hero from "../components/Hero";
+import ProductArticle from "../components/ProductArticle";
+import Button from "../components/Button";
 
 export default function Home() {
 
     return (
         <>
+            <Hero />
+
+            <section className="PopularProducts">
+                <h2>Popular Products</h2>
+                <Button />
+
+
+                <ProductArticle></ProductArticle>
+                <ProductArticle></ProductArticle>
+                <ProductArticle></ProductArticle>
+                <ProductArticle></ProductArticle>
+            </section>
         </>
     )
 }
+
+//ProductArticle skal fetceh data fra en API
