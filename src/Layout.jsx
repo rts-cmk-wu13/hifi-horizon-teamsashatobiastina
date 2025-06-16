056402f770ff5c43c93d376f7e4c54c130c2f3c7
Tobias <tobimation23@gmail.com>
2025-06-16 12:49:17 +0200

    trying to get newsletter to work commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,6 @@
 import Footer from './components/footer';
 import Header from './components/header';
+import Newsletter from './components/Newsletter';
 import './index.css'
 // import Header from './components/Header.jsx'
 // import Footer from './components/Footer.jsx'
