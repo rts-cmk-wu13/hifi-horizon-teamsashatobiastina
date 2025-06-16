a861f4840a7440cd3a6a0729e5bf8a4a10248a61
sasha <sasha.friis@icloud.com>
2025-06-16 15:03:39 +0200

    Fixed button + added functionality
    It now has a prop (color) so you can change the color to the specific
    color it needs to be, it's also no longer self closing so intead of
    <Button text="text here"/>
    it is now
    <Button color="Orange">text here</Button> just like a regular html tag!
    It still needs further styling so it looks a bit nicer but it works as
    intended now.

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,20 @@
-export default function Button({ text}) {
+//komponent:
+export default function Button ({ color="blue", children }) {
+  const colors = {
+    Orange: "bg-BtnOrange text-white hover:bg-blue-700",
+    LightGrey: "bg-BtnLightGrey text-gray-800 hover:bg-gray-300",
+    LightGray: "bg-BtnLightGrey text-gray-800 hover:bg-gray-300",
+    DarkGrey: "bg-BtnDarkGrey text-gray-800 hover:bg-gray-300",
+    DarkGray: "bg-BtnDarkGrey text-gray-800 hover:bg-gray-300",
+    DarkerGrey: "bg-BtnDarkerGrey text-gray-800 hover:bg-gray-300",
+    DarkerGray: "bg-BtnDarkerGrey text-gray-800 hover:bg-gray-300",
+  };
+
   return (
-    <button className={` px-4 py-2 m-5 rounded bg-BtnOrange text-white`}>
-      {text}
+    <button className={`px-4 py-2 m-5 rounded text-white ${colors[color]}`}>
+      {children}{" "}
     </button>
   );
-}
+};
+
+ 
