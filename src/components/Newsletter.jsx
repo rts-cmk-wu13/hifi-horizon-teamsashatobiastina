0a47b8ba1f59531ff09ea2e3bad65aaf0be41acc
Tobias <tobimation23@gmail.com>
2025-06-16 12:49:03 +0200

    added simple newsletter commit

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,23 @@
+export default function Newsletter (){
+
+return(
+<>
+<div>
+<h1>SIGN UP FOR OUR NEWSLETTER</h1>
+<p>Subscribing to our newsletter secures you up to date information about HiFi Horizons latest updates and offers.</p>
+<input type="text" />
+<button>sign up</button>
+
+</div>
+
+
+</>
+
+
+)
+
+
+}
+
+
+
