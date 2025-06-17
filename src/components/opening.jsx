3d668c012326a1dd97e853f48c7f624101b5f6c6
Tobias <tobimation23@gmail.com>
2025-06-17 10:40:43 +0200

    opening component commit

--- src/components/opening.jsx
+++ src/components/opening.jsx
@@ -1,43 +1,51 @@
 export default function Opening() {
   return (
     <>
-      <section>
-        <div>
-        <h1>What we do</h1>
-        <p>We look forward to customising a system to meet your needs.</p>
-        <p>
-          We don’t favour one manufacturer over another – the only thing we do
-          favour is making sure our customers get the right product that suits
-          their needs and listening preferences. We will ask many questions in
-          order to ensure that what you buy from us is tailored to you and you
-          alone.
-        </p>
-        <p>
-          If you are looking for a product not found in our demonstration
-          showrooms or our online site, don’t fret as we have access to hundreds
-          of brands.
-        </p>
-        <p>
-          One of our biggest pleasures of working in this industry is to see the
-          smile on our customers’ faces when they finally hear and see the
-          system of their dreams.
-        </p>
-        </div>
- <h1>Opening hours</h1>
- <address>
-    Edinburgh
-2 Joppa Rd,Edinburgh, EH15 2EU
-Monday to Friday: 10:00am - 5:30pm
-Saturday: 10:00am - 5:30pm
-Sunday: Closed
+  <section className="bg-black text-white px-8 py-25  ">
+  <div className="flex justify-around">
+    {/* Column 1 */}
+    <div className=" space-y-6  w-170 ">
+      <h1 className="text-3xl font-semibold">What we do</h1>
+      <p>We look forward to customising a system to meet your needs.</p>
+      <p>
+        We don’t favour one manufacturer over another – the only thing we do
+        favour is making sure our customers get the right product that suits
+        their needs and listening preferences. We will ask many questions in
+        order to ensure that what you buy from us is tailored to you and you
+        alone.
+      </p>
+      <p>
+        If you are looking for a product not found in our demonstration
+        showrooms or our online site, don’t fret as we have access to hundreds
+        of brands.
+      </p>
+      <p>
+        One of our biggest pleasures of working in this industry is to see the
+        smile on our customers’ faces when they finally hear and see the
+        system of their dreams.
+      </p>
+    </div>
 
- </address>
- <address>Falkirk
-44 Cow Wynd, Falkirk, Central Region, FK1 1PU
-Monday to Friday: 10:00am - 5:30pm
-Saturday - By appointment only
-Sunday: Closed</address>
-      </section>
+    {/* Column 2 */}
+    <div className=" space-y-8">
+      <h1 className="text-3xl font-semibold">Opening hours</h1>
+      <address className="not-italic">
+        <strong>Edinburgh</strong><br />
+        2 Joppa Rd, Edinburgh, EH15 2EU<br />
+        Monday to Friday: 10:00am - 5:30pm<br />
+        Saturday: 10:00am - 5:30pm<br />
+        Sunday: Closed
+      </address>
+      <address className="not-italic">
+        <strong>Falkirk</strong><br />
+        44 Cow Wynd, Falkirk, Central Region, FK1 1PU<br />
+        Monday to Friday: 10:00am - 5:30pm<br />
+        Saturday - By appointment only<br />
+        Sunday: Closed
+      </address>
+    </div>
+  </div>
+</section>
     </>
   );
 }
