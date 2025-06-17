d24d0bad60696571eedb9e96f27a6e7836425625
Tobias <tobimation23@gmail.com>
2025-06-17 10:10:42 +0200

    opening hours commit

---
+++ src/components/opening.jsx
@@ -0,0 +1,43 @@
+export default function Opening() {
+  return (
+    <>
+      <section>
+        <div>
+        <h1>What we do</h1>
+        <p>We look forward to customising a system to meet your needs.</p>
+        <p>
+          We don’t favour one manufacturer over another – the only thing we do
+          favour is making sure our customers get the right product that suits
+          their needs and listening preferences. We will ask many questions in
+          order to ensure that what you buy from us is tailored to you and you
+          alone.
+        </p>
+        <p>
+          If you are looking for a product not found in our demonstration
+          showrooms or our online site, don’t fret as we have access to hundreds
+          of brands.
+        </p>
+        <p>
+          One of our biggest pleasures of working in this industry is to see the
+          smile on our customers’ faces when they finally hear and see the
+          system of their dreams.
+        </p>
+        </div>
+ <h1>Opening hours</h1>
+ <address>
+    Edinburgh
+2 Joppa Rd,Edinburgh, EH15 2EU
+Monday to Friday: 10:00am - 5:30pm
+Saturday: 10:00am - 5:30pm
+Sunday: Closed
+
+ </address>
+ <address>Falkirk
+44 Cow Wynd, Falkirk, Central Region, FK1 1PU
+Monday to Friday: 10:00am - 5:30pm
+Saturday - By appointment only
+Sunday: Closed</address>
+      </section>
+    </>
+  );
+}
