1ca9c72aa7f51b244dc77130f47426f8d6f7c931
Tobias <tobimation23@gmail.com>
2025-06-19 10:42:02 +0200

    added the product specifics component

---
+++ src/components/Product_specific.jsx
@@ -0,0 +1,61 @@
+export default function Specific() {
+  return (
+    <>
+    <div className="my-8 text-3xl font-semibold uppercase">
+<h1>product specifications</h1>
+</div>
+<table class="table-auto w-4/10 text-center ml-20 border-separate border-spacing-0">
+  <tbody class="divide-y divide-x  divide-gray-200 ">
+    <tr class="odd:bg-BtnLightGrey ">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+    <tr class="odd:bg-gray-100">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+        <tr class="odd:bg-BtnLightGrey">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+    <tr class="odd:bg-gray-100">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+ 
+     <tr class="odd:bg-BtnLightGrey">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+    <tr class="odd:bg-gray-100">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+     <tr class="odd:bg-BtnLightGrey">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+    <tr class="odd:bg-gray-100">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+     <tr class="odd:bg-BtnLightGrey">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+    <tr class="odd:bg-gray-100">
+      <td class="w-4/10 p-2 font-bold border-r-2 border-gray-300">xxxxxx</td>
+      <td class="p-2">xxxxxx</td>
+    </tr>
+     
+ 
+ 
+ 
+
+  </tbody>
+</table>
+
+
+    </>
+  );
+}
