b4977cd9e8e1d82789f669d68c4e1ab02bef390e
Tobias <tobimation23@gmail.com>
2025-06-19 09:06:08 +0200

    changed some tailwind in here

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,24 +2,24 @@ export default function ContactForm() {
   return (
     <>
 
-    <h1 className="mx-16 pt-16 text-7xl font-semibold text-BtnDarkerGrey uppercase mb-12">Get in touch with us</h1>
-    <div className="bg-ElmBg p-10 mx-7   ">
+
+    <div className="bg-ElmBg p-12   ">
       <form className="flex flex-col gap-6 text-lg font-bold uppercase " action="" method="get">
-        <div className="flex-col  flex gap-4">
-          <label className="block" htmlFor="name  ">Full name * </label>
-          <input className="bg-BtnLightGrey shadow-(--prodArticle) block w-[67%] h-10 " type="text" name="name" id="name" required />
+        <div className="flex-col  flex gap-2">
+          <label className="block" htmlFor="name  ">Full name <span className="text-red-500">*</span> </label>
+          <input className="bg-BtnLightGrey shadow-(--prodArticle) block w-[67%] h-12 " type="text" name="name" id="name" required />
         </div>
-        <div className="flex-col  flex gap-4">
-          <label className="block" htmlFor="Email">Email * </label>
-          <input className="bg-BtnLightGrey shadow-(--prodArticle) block w-[67%] h-10" type="email" name="email" id="email" required />
+        <div className="flex-col  flex gap-2">
+          <label className="block" htmlFor="Email">Email <span className="text-red-500">*</span> </label>
+          <input className="bg-BtnLightGrey shadow-(--prodArticle) block w-[67%] h-12" type="email" name="email" id="email" required />
         </div>
-  <div className="flex-col  flex gap-4">
-  <label className="block" htmlFor="subject"> subject *</label>
-  <input className="bg-BtnLightGrey shadow-(--prodArticle) block w-[67%] h-10"   id="subject" name="subject"  required />
+  <div className="flex-col  flex gap-2">
+  <label className="block" htmlFor="subject"> subject <span className="text-red-500">*</span></label>
+  <input className="bg-BtnLightGrey shadow-(--prodArticle) block w-[67%] h-12"   id="subject" name="subject"  required />
 </div>
-     <div className="flex-col  flex gap-4 ">
-  <label className="block" htmlFor="message">message *</label>
-  <textarea className="bg-BtnLightGrey shadow-(--prodArticle) block w-[90%] h-40"   id="message" name="message" rows="5" cols="33" required />
+     <div className="flex-col  flex gap-2 ">
+  <label className="block" htmlFor="message">message <span className="text-red-500">*</span></label>
+  <textarea className="bg-BtnLightGrey shadow-(--prodArticle) block w-full h-40"   id="message" name="message" rows="5" cols="33" required />
 </div>
 <div className="flex justify-end">
 
