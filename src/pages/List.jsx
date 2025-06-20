6f70008e5e826aa4f58af42c883e057867816853
Tobias <tobimation23@gmail.com>
2025-06-20 12:44:49 +0200

    added some css

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -78,9 +78,16 @@ export default function List() {
   };
 
   return (
-    <section>
+    <section className="grid grid-cols-4 p-12 bg-BgSlightGrey gap-x-8">
+                     <h2 className="col-span-4 text-7xl text-BtnDarkerGrey font-bold mb-10 uppercase">
+                    Products
+                </h2>
+                <div className="">
+                    <h1>sort by</h1>
       <Sorting filterProduct={filterProduct} activeCategory={activeCategory} />
-      <ul>
+      </div>
+                <div className="col-span-3">
+      <ul className="grid grid-cols-3 gap-6">
         {filtered.map((product) => (
           <li key={product.id}>
             <ProductArticle
@@ -91,7 +98,8 @@ export default function List() {
             />
           </li>
         ))}
-      </ul>
+     </ul> </div>
     </section>
   );
 }
+
