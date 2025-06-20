f9cdcada26d066c22cdd0f6e85ec81a8aad66b8d
Tobias <tobimation23@gmail.com>
2025-06-20 10:18:45 +0200

    added sorting

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -1,11 +1,30 @@
-export default function Opening() {
+export default function Sorting({ filterProduct, activeCategory }) {
   return (
-<>
-
-<div id="buttons">
-<button className="button-value " onClick={filterProducts('')}></button>
-</div>
-
-</>
-  )
-  }
+    <div id="buttons">
+      <button
+        className={`button-value ${activeCategory === "All" ? "active" : ""}`}
+        onClick={() => filterProduct("All")}
+      >
+        All
+      </button>
+      <button
+        className={`button-value ${activeCategory === "creek" ? "active" : ""}`}
+        onClick={() => filterProduct("creek")}
+      >
+        Creek
+      </button>
+      <button
+        className={`button-value ${activeCategory === "parasound" ? "active" : ""}`}
+        onClick={() => filterProduct("parasound")}
+      >
+        Parasound
+      </button>
+      <button
+        className={`button-value ${activeCategory === "manley" ? "active" : ""}`}
+        onClick={() => filterProduct("manley")}
+      >
+        Manley
+      </button>
+    </div>
+  );
+}
