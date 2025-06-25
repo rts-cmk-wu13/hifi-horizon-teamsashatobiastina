58605e0807a968c6a39355d5bd54e19ae51e8f97
Tina Wulff <tina@tinawulff.dk>
2025-06-25 11:01:47 +0200

    profileside oprettet router opdateret

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
+import { useAuth } from "../contexts/Authcontext";
 
 export default function Header() {
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
+    const { user } = useAuth(); 
 
   const handleInput = (e) => {
     setQuery(e.target.value);
   };
 
-    const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // If already on /produkter, update the query param
     if (location.pathname === "/produkter") {
@@ -21,33 +23,43 @@ export default function Header() {
     }
   };
 
+
+
+  const handleProfileClick = () => {
+    if (user) {
+      navigate("/profile");
+    } else {
+      navigate("/login", { state: { from: { pathname: "/profile" } } });
+    }
+  };
+
   return (
     <div className="bg-black text-white px-8 py-6">
       <nav className="flex justify-between items-center font-light text-xs">
 
         <div className="flex items-center gap-5">
-<NavLink to="/">
-          <figure className="flex items-center">
-            <img    src="public/logo_sml 1.svg" alt="Logo" className="w-16" />
-          </figure>
-</NavLink>
-<ul className="flex gap-8 tracking-wide uppercase overflow-visible">
+          <NavLink to="/">
+            <figure className="flex items-center">
+              <img src="public/logo_sml 1.svg" alt="Logo" className="w-16" />
+            </figure>
+          </NavLink>
+          <ul className="flex gap-8 tracking-wide uppercase overflow-visible">
 
- <li className="relative group cursor-pointer">
-  <NavLink to="/produkter" className="hover:underline pb-15 pr-2">Shop</NavLink>
+            <li className="relative group cursor-pointer">
+              <NavLink to="/produkter" className="hover:underline pb-15 pr-2">Shop</NavLink>
 
-  <div className="absolute hidden group-hover:flex flex-col top-16 left-0 bg-white text-black py-5  px-8 shadow-lg z-50 w-74">
-    <h3 className="font-bold text-lg mb-4">Browse Categories</h3>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1 text-lg">CD Players</a>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">DVD Players</a>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Preamps</a>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Speakers</a>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Turntables</a>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Integrated Amplifiers</a>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Power Amplifiers</a>
-    <a href="#" className="text-[#8a6d3b] hover:underline py-1 text-lg">Tube Amplifiers</a>
-  </div>
-</li>
+              <div className="absolute hidden group-hover:flex flex-col top-16 left-0 bg-white text-black py-5  px-8 shadow-lg z-50 w-74">
+                <h3 className="font-bold text-lg mb-4">Browse Categories</h3>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1 text-lg">CD Players</a>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">DVD Players</a>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Preamps</a>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Speakers</a>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Turntables</a>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Integrated Amplifiers</a>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1  text-lg">Power Amplifiers</a>
+                <a href="#" className="text-[#8a6d3b] hover:underline py-1 text-lg">Tube Amplifiers</a>
+              </div>
+            </li>
 
             <li className="cursor-pointer hover:underline">
               <NavLink to="/about">About Us</NavLink>
@@ -61,23 +73,23 @@ export default function Header() {
 
         <div className="flex items-center gap-10">
           {/* Search */}
- <form onSubmit={handleSubmit} className="flex items-center space-x-2">
-      <label htmlFor="product" className="flex items-center cursor-pointer">
-        <FaSearch />
-      </label>
-      <input
-        type="search"
-        id="product"
-        name="q"
-        className="px-2 py-1 text-black bg-white"
-        placeholder="Search a product"
-        value={query}
-        onChange={handleInput}
-      />
-    </form>
+          <form onSubmit={handleSubmit} className="flex items-center space-x-2">
+            <label htmlFor="product" className="flex items-center cursor-pointer">
+              <FaSearch />
+            </label>
+            <input
+              type="search"
+              id="product"
+              name="q"
+              className="px-2 py-1 text-black bg-white"
+              placeholder="Search a product"
+              value={query}
+              onChange={handleInput}
+            />
+          </form>
 
           <div className="flex gap-3">
-            <FaUser className="cursor-pointer" size={20} />
+            <FaUser className="cursor-pointer" size={20} onClick={handleProfileClick} />
             <div className="relative cursor-pointer mr-10">
               <FaShoppingCart size={20} />
             </div>
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,20 @@
+export default function Profile() {
+
+    return (
+        <section>
+            <header>
+                <nav>
+                    <ul>
+                        <li>Profile</li>
+                        <li>Orders</li>
+                    </ul>
+                </nav>
+            </header>
+
+            
+
+
+        </section>
+        
+    )
+}
